ba7ed0f01cb4a0164dfb742f82102a2152795aef
jeetguptaadvantal <47409309+jeetguptaadvantal@users.noreply.github.com>
2019-02-21 19:14:12 +0530

    Add files via upload

---
+++ src/main/java/com/advantal/CarbonMobileApiApplication.java
@@ -0,0 +1,13 @@
+package com.advantal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonMobileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonMobileApiApplication.class, args);
+	}
+
+}
