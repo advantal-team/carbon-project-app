e2231620a026914a2f92edbe65bf74c2d4556a13
Rupesh <Rupesh@Dell>
2019-02-27 11:26:30 +0530

    Committed code for Creating web service structure

---
+++ src/main/java/com/advantal/model/User.java
@@ -0,0 +1,140 @@
+package com.advantal.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="USER")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="user_id")
+	
+	private Integer userId;
+	
+	@Column(name="user_name")
+	
+	private String userName;
+	
+	@Column(name="mobile_no")
+	
+	private Integer mobileNo;
+	
+	@Column(name="password_1")
+	
+	private String password1;
+	
+	@Column(name="password_2")
+	
+	private String password2;
+	
+	@Column(name="user_role")
+	
+	private String userRole;
+	
+	@CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+	@Column(name="added_date")
+	
+	private Date addedDate;
+	
+	@UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+	@Column(name="update_date")
+	
+	private Date updateDate;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "module_id", nullable = false)
+    private Module module;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Integer getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(Integer mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getPassword1() {
+		return password1;
+	}
+
+	public void setPassword1(String password1) {
+		this.password1 = password1;
+	}
+
+	public String getPassword2() {
+		return password2;
+	}
+
+	public void setPassword2(String password2) {
+		this.password2 = password2;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Module getModule() {
+		return module;
+	}
+
+	public void setModule(Module module) {
+		this.module = module;
+	}
+	
+	
+	
+}
+	
---
+++ src/main/java/com/advantal/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.advantal.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.advantal.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	
+
+}
